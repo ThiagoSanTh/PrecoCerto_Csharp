471aea923ebec75d2607977ec24c296140a7481e
ThiagoSanth <thiagoalmeidasantanax@gmail.com>
2026-04-07 15:08:43 -0300

    SQL implementado

---
+++ BackEnd/Preco Certo/Pc.Infraestrutura/AppDbContext.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Pc.Dominio.Entities.Catalogo;
+using Pc.Dominio.Entities.Estabelecimentos;
+using Pc.Dominio.Entities.Interacoes;
+using Pc.Dominio.Entities.Usuarios;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pc.Infraestrutura
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Cliente> Clientes { get; set; }
+        public DbSet<Lojista> Lojistas { get; set; }
+        public DbSet<Loja> Lojas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<Produto> Produtos { get; set; }
+        public DbSet<Oferta> Ofertas { get; set; }
+        public DbSet<Favorito> Favoritos { get; set; }
+        public DbSet<HistoricoPesquisa> HistoricosPesquisa { get; set; }
+        public DbSet<Avaliacao> Avaliacoes { get; set; }
+        public DbSet<PreferenciaCliente> PreferenciasClientes { get; set; }
+    }
+}
+
--- BackEnd/Preco Certo/Pc.WebApi/Program.cs
+++ BackEnd/Preco Certo/Pc.WebApi/Program.cs
@@ -1,17 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using Pc.Infraestrutura;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
@@ -20,4 +26,5 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+app.Run();
 app.Run();
